18b92063527347c16ab427368e0d80797a2a779a
rahul-dudhrejiya <dudharejiya2499@email.com>
2026-05-30 11:19:14 +0530

    fix: Update CORS origin to use the correct production URL

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ app.use(cors({
     origin: [
         "http://localhost:5173",
         "http://localhost:5174",
-        "https://cravio.vercel.app",
+        "https://cravio-food-delivery.vercel.app/",
         "https://cravio-admin.vercel.app"
     ],
     credentials: true
